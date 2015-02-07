d03fb906055384d3b6887b2a19510d73a37eb7e1
deepti <deepti.kochar12@gmail.com>
2015-02-06 17:10:29 -0800

    changes in dining, README

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # GTThreads
 Advanced Operating Systems Project
+
+Linux Platform: Ubuntu 14.04
+
+Compilation:
+In the main directory, type "make".
+
+This will give gtthread.a and dining_philosophers.
+
+Run:
+
+To run the dining philosophers program type:
+./dining_philosophers
+
+Premptive scheduler implementation:
+
+I have created a queue data structure which stores thread ids and contexts. When a thread is created or swapped out, it is inserted at the end of the queue. The thread at the start of the queue is dequeued and swapped in by the scheduler.
+
+I've used setitimer to generate SIGVTALRM signals at time intervals specified by the input to gtthread_init. I have specified the function gtthread_scheduler as the signal handler. When a signal comes in, the scheduler is called. It dequeues the next thread and enqueues the current one. It then swaps the contexts of these threads.
+
+The scheduler is also called with argument -1 when a thread exits. In this case, it does not enqueue the thread again. It also doesn't swapcontext. Instead it uses setcontext to set the context to the next thread's.
+
+
+Dining Philosophers:
+
+To implement the dining philosophers problem, I've used one mutex for each chopstick. The philosopher's eat and think for random amounts of time.
+
+To prevent deadlock, each philosopher must first pick up the chopstick with the lower number. For all except one philosopher, this will be the left chopstick. Only the last philosopher will pick up his right chopstick first (his left chopstick is #5 while the right one is #1). This ensures that even if the first 4 philosopher's pick up their left chopsticks simultaneously, there will be one chopstick left on the table since this is the highest numbered chopstick. Therefore it can be picked up by the fourth philosopher who can eat and then release both his chopsticks which can be picked up by the next philosopher.
+
+
--- dining_philosophers.c
+++ dining_philosophers.c
@@ -1,25 +1,51 @@
 
-#include <gtthread.h>
+#include "gtthread.h"
 
 #define NUM_CHOPSTICKS 5
 #define MAX_DURATION 100000000
 
-int chopstick[NUM_CHOPSTICKS];
-gtthread_mutex_t pick_up_chopsticks;
+gtthread_mutex_t chopstick_mutex[NUM_CHOPSTICKS];
+/*gtthread_mutex_t pick_up_chopsticks;*/
 
 
 void think(int duration, int phil_num)
 {
-	printf("Philosopher %d is thinking\n", phil_num);
 	int i;
+	printf("Philosopher %d is thinking\n", phil_num);
 	for(i = 0; i < duration; i++);
 }
 
 void eat(int duration, int phil_num)
 {
-	printf("Philosopher %d is hungry\n", phil_num);
+	int i;
 	int index1 = phil_num - 1;
 	int index2 = phil_num % NUM_CHOPSTICKS;
+	printf("Philosopher %d is hungry\n", phil_num);	
+
+	if(index1 < index2)
+	{
+		gtthread_mutex_lock(&chopstick_mutex[index1]);
+		printf("Philosopher %d has picked up left chopstick\n", phil_num);
+		gtthread_mutex_lock(&chopstick_mutex[index2]);
+		printf("Philosopher %d has picked up right chopstick\n", phil_num);
+	}
+	else
+	{
+		gtthread_mutex_lock(&chopstick_mutex[index2]);
+		printf("Philosopher %d has picked up right chopstick\n", phil_num);
+		gtthread_mutex_lock(&chopstick_mutex[index1]);
+		printf("Philosopher %d has picked up left chopstick\n", phil_num);
+	}
+
+	printf("Philosopher %d is eating\n", phil_num);
+	for(i = 0; i < duration; i++);
+
+	gtthread_mutex_unlock(&chopstick_mutex[index1]);
+	printf("Philosopher %d has released left chopstick\n", phil_num);
+	gtthread_mutex_unlock(&chopstick_mutex[index2]);	
+	printf("Philosopher %d has released right chopstick\n", phil_num);
+
+	/*
 	while(1)
 	{
 		gtthread_mutex_lock(&pick_up_chopsticks);
@@ -34,8 +60,7 @@ void eat(int duration, int phil_num)
 		gtthread_mutex_unlock(&pick_up_chopsticks);
 		gtthread_yield();
 	}
-	int i;
-	for(i = 0; i < duration; i++);
+	
 
 	gtthread_mutex_lock(&pick_up_chopsticks);
 	chopstick[index1] = 1;
@@ -43,11 +68,12 @@ void eat(int duration, int phil_num)
 	printf("Philosopher %d has released left chopstick\n", phil_num);
 	printf("Philosopher %d has released right chopstick\n", phil_num);
 	gtthread_mutex_unlock(&pick_up_chopsticks);
+	*/
 
 	return;
 }
 
-int philosopher(int phil_num)
+void* philosopher(int phil_num)
 {
 	int duration;
 	srand(time(NULL));
@@ -60,6 +86,7 @@ int philosopher(int phil_num)
 		duration = rand() % MAX_DURATION;
 		eat(duration, phil_num);
 	}
+	return NULL;
 }
 
 int main()
@@ -68,19 +95,20 @@ int main()
 	long period = 1000;
 	gtthread_init(period);
 	gtthread_t philosopher_thread[NUM_CHOPSTICKS];
-	gtthread_mutex_init(&pick_up_chopsticks);
+	/*gtthread_mutex_init(&pick_up_chopsticks);*/
 
 	for (i = 0; i < NUM_CHOPSTICKS; i++)
 	{
-		chopstick[i] = 1;
+		gtthread_mutex_init(&chopstick_mutex[i]);
 	}
 
 	for(i = 0; i < NUM_CHOPSTICKS; i++)
 	{
-		if(gtthread_create(&philosopher_thread[i], &philosopher, i+1) < 0)
+
+		if(gtthread_create(&philosopher_thread[i], philosopher, (i+1) ) < 0)
 			return -1;
 	}
-	void *retval;
-	gtthread_join(philosopher_thread[0], &retval);
+	
+	gtthread_join(philosopher_thread[0], NULL);
 	return 0;
 }
